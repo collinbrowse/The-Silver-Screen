36966699a5cebf7f01c26861d5da6dc0ef182b56
Collin Browse <cbrowse13@gmail.com>
2026-09-09 10:15:10 -0400

    Revert "Keep Favorites navigation title inline during search."
    This reverts commit 5e6a0edacb432150b7a452b9caa0781f7d376d29.

--- URBNFlicks/App/RootTabView.swift
+++ URBNFlicks/App/RootTabView.swift
@@ -88,7 +88,6 @@ private struct FavoritesTabRoot: View {
                 imageLoader: imageLoader
             )
             .navigationTitle("Favorites")
-            .navigationBarTitleDisplayMode(.inline)
             .navigationDestination(for: Route.self) { route in
                 AppRouteDestination(
                     route: route,
